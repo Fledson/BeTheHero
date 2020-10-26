a9d210205b964f8ca5710b9adf3ea1f99f1ca0b3
Fledson Henrique <fledsonhenrique@gmail.com>
2020-10-26 00:54:21 -0300

    Inicio do Frontend, Pagina de Login criada!

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {} from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi'
+import './styles.css'
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
+
+export default function Logon () {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero"/>
+
+        <form action="">
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID"/>
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041"/>
+            Não tenho Cadastro  
+          </a>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt="Heroes"/>
+    </div>
+  )
+}
